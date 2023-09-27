5b14978e3ab2cc952c8bf1c796586a12ea43368c
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-09-27 13:36:05 +0300

    Add useHash to router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { CartComponent } from './cart/cart.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
-    ]),
+    ], { useHash: true }),
   ],
   declarations: [
     AppComponent,
